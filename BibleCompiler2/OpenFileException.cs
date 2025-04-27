141ad62676338e1de1a7f8a6e59b0a9341e59a61
Nathan Talbot <nathant409@gmail.com>
2025-04-27 17:39:44 -0400

    refactor: update err method to accept processName parameter and remove process termination logic

--- BibleCompiler2/OpenFileException.cs
+++ BibleCompiler2/OpenFileException.cs
@@ -10,18 +10,10 @@ namespace BibleCompiler2
 {
     internal class OpenFileException
     {
-        public void err(string fileName)
+        public void err(string fileName, string processName = "winWord")
         {
             MessageBox.Show($"Your \"{fileName}\" Document is Open,\nPlease Save And Close Your Document", "Open Document Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-            string processName = "winWord";
             Process[] processList = Process.GetProcesses();
-            foreach (Process theProcess in processList)
-            {
-                if (theProcess.ProcessName.Contains(processName))
-                {
-                    theProcess.Kill();
-                }
-            }
         }
 
     }
